73b48831ab5be071f4293dbaea48887db872134c
GroenendijkMax <max.groenendijk@kadaster.nl>
2026-04-21 12:23:44 +0200

    TMS-10879 - ggc-cesium - ggc-home

--- projects/ggc-home/src/app/examples/example-toolbar/example-toolbar/example-toolbar.component.spec.ts
+++ projects/ggc-home/src/app/examples/example-toolbar/example-toolbar/example-toolbar.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ExampleToolbar } from "./example-toolbar-location.component";
+import { ExampleToolbar } from "./example-toolbar.component";
 
 describe("ExampleSearchLocationComponent", () => {
   let component: ExampleToolbar;
