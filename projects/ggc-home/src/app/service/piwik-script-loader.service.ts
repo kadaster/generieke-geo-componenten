bee21d99d29e65d5450d0593cd4c4ae260f13b64
Dennis <dennis.geerdink@kadaster.nl>
2026-05-06 11:27:41 +0200

    piwik script verplaatst

--- projects/ggc-home/src/app/service/piwik-script-loader.service.ts
+++ projects/ggc-home/src/app/service/piwik-script-loader.service.ts
@@ -11,12 +11,15 @@ export class PiwikScriptLoaderService {
     const piwikScriptElement = document.getElementById(
       "piwik-script"
     ) as HTMLScriptElement;
+    console.log("piwikScriptElement: ", piwikScriptElement);
 
     // Set the correct piwik-script path from the environment file
     if (piwikScriptElement) {
       piwikScriptElement.src =
         "./assets/piwik-script/" + environment.piwikScript;
       piwikScriptElement.async = true;
+      console.log("piwikScript: ", piwikScriptElement.src);
+
     }
   }
 }
