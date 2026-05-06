8cd826a1c60ab49f022d68156db1de3a9a2c2160
VelsLinda <linda.vels@kadaster.nl>
2026-05-06 09:07:59 +0200

    TMS-10895 voorbeeld kaarteigenschappen tonen

--- projects/ggc-home/src/app/examples/example-map/example-map-zoom-scale-position/example-map-zoom-scale-position.component.ts
+++ projects/ggc-home/src/app/examples/example-map/example-map-zoom-scale-position/example-map-zoom-scale-position.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit } from "@angular/core";
-import { GgcMapComponent } from "@kadaster/ggc-map";
+import { Component, inject, OnInit } from "@angular/core";
+import {
+  GgcMapComponent,
+  GgcMapDetailsContainerComponent,
+  GgcMapService,
+  GgcMousePositionComponent,
+  GgcScaleDenominatorComponent,
+  GgcScaleLineComponent,
+  GgcZoomLevelComponent
+} from "@kadaster/ggc-map";
 import { Webservice } from "../../../../../../ggc-cesium/src/lib/model/interfaces";
 import { ExampleFormatComponent } from "../../example-format/example-format.component";
 import { ComponentInfo } from "../../component-info.model";
@@ -9,7 +17,18 @@ import { Tags } from "../../tags.enum";
 
 @Component({
   selector: "app-example-search-location",
-  imports: [GgcMapComponent, ExampleFormatComponent],
+  imports: [
+    GgcMapComponent,
+    GgcScaleLineComponent,
+    GgcScaleDenominatorComponent,
+    GgcMapDetailsContainerComponent,
+    GgcMousePositionComponent,
+    ExampleFormatComponent,
+    GgcScaleDenominatorComponent,
+    GgcZoomLevelComponent,
+    GgcMousePositionComponent,
+    GgcMousePositionComponent
+  ],
   templateUrl: "./example-map-zoom-scale-position.component.html",
   styleUrl: "./example-map-zoom-scale-position.component.scss"
 })
@@ -28,17 +47,20 @@ export class ExampleMapZoomScalePositionComponent
     imageLocation:
       "code/examples/example-map/example-map-zoom-scale-position/example-map-zoom-scale-position.png"
   } as ComponentInfo;
+
   urlComponentModule =
-    "/example-map/example-map-zoom-scale-position/example-map-zoom-scale-position.component.ts";
+    "example-map/example-map-zoom-scale-position/example-map-zoom-scale-position.component.ts";
   tsDocsUrl = `${document.baseURI}tsdocs/modules/ggc-map_src_public-api.html`;
   // DOCS-SKIP:END
+
   protected mapConfig: Webservice[];
+  private readonly mapService = inject(GgcMapService);
 
   constructor() {
     super();
   }
-
   ngOnInit() {
+    this.mapService.getMap().getView().setConstrainResolution(true);
     this.httpClient
       .get(
         "code/examples/example-map/example-map-zoom-scale-position/kaartconfig.json"
