e8c1410067026b0150ad7c99b2c85a53f454ffc5
Jordy Mackay <JordyMackay@users.noreply.github.com>
2026-05-19 06:49:38 +0200

    URLs naar GGC Home en releases bijgewerkt (#32)
    * URLs naar GGC Home bijgewerkt
    
    * link voor releases aangepast naar GitHub tags
    
    ---------
    
    Co-authored-by: Jordy Mackay <jordy.mackay@kadaster.nl>

--- projects/ggc-home/src/app/constants/urls.ts
+++ projects/ggc-home/src/app/constants/urls.ts
@@ -1,5 +1,5 @@
 export const githubUrl =
   "https://github.com/kadaster/generieke-geo-componenten";
-export const githubReleasesUrl = githubUrl + "/releases";
+export const githubReleasesUrl = githubUrl + "/tags";
 
 export const tsdocsUrl = `${document.baseURI}tsdocs/index.html`;
