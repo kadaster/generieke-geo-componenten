d22b57b6aeb0002034bc96bb3317f2524f1a7be6
Pim <pim.vanderburgt@kadaster.nl>
2026-05-28 14:11:46 +0200

    TMS-11205 toevoegen voorbeeld 3D basic

---
+++ projects/ggc-home/src/app/examples/example-3d/example-3d-basic/example-3d-basic.component.ts
@@ -0,0 +1,83 @@
+import { Component, inject, ViewEncapsulation } from "@angular/core";
+import { ComponentInfo } from "../../component-info.model";
+import { Components } from "../../components.enum";
+import { Themes } from "../../themes.enum";
+import { Tags } from "../../tags.enum";
+import {
+  CameraOptions,
+  cameraOptionsTorentjeDenHaag,
+  GgcControlsComponent,
+  GgcViewerComponent,
+  ViewerOptions,
+  Webservice
+} from "@kadaster/ggc-cesium";
+import { ExampleFormatComponent } from "../../example-format/example-format.component";
+import * as proj4x from "proj4";
+import { HttpClient } from "@angular/common/http";
+import { register } from "ol/proj/proj4";
+import { defs } from "@kadaster/ggc-models";
+
+const proj4 = (proj4x as any).default;
+
+@Component({
+  selector: "app-example-3d-basic",
+  templateUrl: "./example-3d-basic.component.html",
+  styleUrl: "./example-3d-basic.component.scss",
+  imports: [GgcViewerComponent, GgcControlsComponent, ExampleFormatComponent],
+  encapsulation: ViewEncapsulation.None
+})
+export class Example3dBasicComponent {
+  // DOCS-SKIP:START
+  readonly componentInfo: ComponentInfo = {
+    route: "/example-3d-basic",
+    title: "3D Kaart tonen",
+    introduction: "Toon een 3D kaart basic voorbeeld.",
+    components: [Components.GGC_3D],
+    theme: [Themes.KAARTLAGEN],
+    tags: [Tags.LAYER],
+    imageLocation:
+      "code/examples/example-3d/example-3d-basic/example-3d-basic.png"
+  } as ComponentInfo;
+
+  urlComponentModule =
+    "example-3d/example-3d-basic/example-3d-basic.component.ts";
+  tsDocsUrl = `${document.baseURI}tsdocs/modules/ggc-map_src_public-api.html`;
+  // DOCS-SKIP:END
+
+  protected hideLogo = false;
+  protected cameraOptions: CameraOptions;
+  protected webService: Webservice[];
+  protected viewerOptions: ViewerOptions = {
+    elementId: "cesium-basic",
+    terrainModelUrl:
+      "https://api.pdok.nl/kadaster/3d-basisvoorziening/ogc/v1/collections/digitaalterreinmodel/quantized-mesh",
+    directionalLightOptions: {
+      direction: "cameraDirection",
+      intensity: 2.5
+    }
+  };
+  protected kaartConfig =
+    "code/examples/example-3d/example-3d-basic/kaartconfig.json";
+
+  private readonly httpClient = inject(HttpClient);
+
+  constructor() {
+    proj4.defs("EPSG:28992", defs);
+    register(proj4);
+
+    this.httpClient.get(this.kaartConfig).subscribe((data) => {
+      this.webService = data as Webservice[];
+    });
+  }
+
+  public onCesiumReady() {
+    // zoom to Torentje
+    setTimeout(() => {
+      this.cameraOptions = cameraOptionsTorentjeDenHaag;
+    });
+  }
+
+  protected toggleHideLogo() {
+    this.hideLogo = !this.hideLogo;
+  }
+}
--- projects/ggc-home/src/app/examples/example-index/example-index.component.ts
+++ projects/ggc-home/src/app/examples/example-index/example-index.component.ts
@@ -36,6 +36,7 @@ import { Tags } from "../tags.enum";
 import { Components } from "../components.enum";
 import { Themes } from "../themes.enum";
 import { SortPipe } from "../../pipes/sort.pipe";
+import { Example3dBasicComponent } from "../example-3d/example-3d-basic/example-3d-basic.component";
 
 interface GroupedCards {
   theme: string;
@@ -94,13 +95,14 @@ export class ExampleIndexComponent {
     new ExampleDrawCenterDrawComponent().componentInfo,
     new ExampleMeasure().componentInfo,
     new ExampleMeasureOwnStyleLabel().componentInfo,
-    new ExampleMapZoomScalePositionComponent().componentInfo
+    new ExampleMapZoomScalePositionComponent().componentInfo,
+    new Example3dBasicComponent().componentInfo
   ];
 
-  private selectedComponentsKey = "selectedComponents";
-  private selectedTagsKey = "selectedTags";
-  private selectedThemesKey = "selectedThemes";
-  private searchTermKey = "searchTerm";
+  private readonly selectedComponentsKey = "selectedComponents";
+  private readonly selectedTagsKey = "selectedTags";
+  private readonly selectedThemesKey = "selectedThemes";
+  private readonly searchTermKey = "searchTerm";
 
   constructor() {
     const storedSelectedThemes = sessionStorage.getItem(this.selectedThemesKey);
