c89e02d0dc5ae4c62505225ed7f17805512ab495
VelsLinda <linda.vels@kadaster.nl>
2026-06-01 14:59:56 +0200

    TMS-10980 juiste import

--- projects/ggc-feature-info/src/lib/service/feature-info-event.service.ts
+++ projects/ggc-feature-info/src/lib/service/feature-info-event.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
-import { FeatureInfoComponentEvent } from "@kadaster/ggc-feature-info";
+import { FeatureInfoComponentEvent } from "../model/feature-info-component-event";
 
 @Injectable({
   providedIn: "root"
