c9692768094d0005f73057aad9794ce9fe15ff33
Dennis <dennis.geerdink@kadaster.nl>
2026-06-03 12:10:18 +0200

    Vitest config centraal werkend

---
+++ vitest.config.ts
@@ -0,0 +1,67 @@
+import { defineConfig } from "vitest/config";
+import path from "node:path";
+const resolveOLMock = (p: string) =>
+  path.resolve(__dirname, `src/test/mocks/open-layers/${p}`);
+
+console.log("Vitest configuration started");
+export default defineConfig({
+  resolve: {
+    alias: [
+      { find: /^ol\/Map$/, replacement: resolveOLMock("Map.ts") },
+      { find: /^ol\/Feature$/, replacement: resolveOLMock("Feature.ts") },
+      {
+        find: /^ol\/style\/Text$/,
+        replacement: resolveOLMock("style/Text.ts")
+      },
+      {
+        find: /^ol\/style\/Fill$/,
+        replacement: resolveOLMock("style/Fill.ts")
+      },
+      {
+        find: /^ol\/style\/Stroke$/,
+        replacement: resolveOLMock("style/Stroke.ts")
+      },
+      {
+        find: /^ol\/style\/Circle$/,
+        replacement: resolveOLMock("style/Circle.ts")
+      },
+      {
+        find: /^ol\/style\/RegularShape$/,
+        replacement: resolveOLMock("style/RegularShape.ts")
+      },
+      {
+        find: /^ol\/style\/Icon$/,
+        replacement: resolveOLMock("style/Icon.ts")
+      },
+      { find: /^ol\/style\/.+$/, replacement: resolveOLMock("style/Style.ts") },
+      {
+        find: /^ol\/geom\/.+$/,
+        replacement: resolveOLMock("geom/Geometry.ts")
+      },
+      {
+        find: "@kadaster/ggc-cesium",
+        replacement: path.resolve(__dirname, "src/test/mocks/cesium/Cesium.ts")
+      }
+    ]
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["src/test.ts", "src/test/setup-tests.ts"],
+    server: {
+      // deps: {
+      //   inline: [/^ol/, /^@kadaster\/generieke-geo-componenten-/]
+      // }
+    },
+    coverage: {
+      provider: "istanbul",
+      reporter: ["text", "html", "lcov"],
+      reportsDirectory: "./coverage",
+      exclude: ["src/test/**", "**/*.spec.ts"]
+    }
+  },
+  ssr: { noExternal: true },
+  optimizeDeps: {
+    exclude: ["ol"]
+  }
+});
