802d89824fb1a0875d74be36034e3ff394c6b410
GroenendijkMax <max.groenendijk@kadaster.nl>
2026-06-11 08:43:32 +0200

    TMS-11187

--- projects/ggc-legend/src/lib/legend-url/ggc-legend-url.component.ts
+++ projects/ggc-legend/src/lib/legend-url/ggc-legend-url.component.ts
@@ -1,5 +1,15 @@
-import { Component, Input } from "@angular/core";
+import {
+  Component,
+  computed,
+  effect,
+  inject,
+  Input,
+  signal
+} from "@angular/core";
 import { LegendUrl } from "@kadaster/ggc-models";
+import { HttpClient } from "@angular/common/http";
+import { DomSanitizer } from "@angular/platform-browser";
+import DOMPurify from "dompurify";
 
 @Component({
   selector: "ggc-legend-url",
@@ -14,4 +24,38 @@ export class GgcLegendUrlComponent {
    * De naam van de laag die wordt weergegeven. Deze wordt gebruikt als alternatieve tekst.
    */
   @Input() layerName: string;
+
+  /**
+   * Checkt of de legenda url een svg is.
+   */
+  isSvg = computed(
+    () =>
+      this.legend?.legendUrl && /\.svg(?:\?.*)?$/i.test(this.legend.legendUrl)
+  );
+
+  /**
+   * Indien svg wordt weergegeven, bevat deze de veilige svg content.
+   */
+  svgContent = signal<string>("");
+
+  private readonly httpClient = inject(HttpClient);
+  private readonly sanitizer = inject(DomSanitizer);
+
+  constructor() {
+    effect(() => {
+      this.svgContent.set("");
+      const url = this.legend.legendUrl;
+
+      if (!url || !this.isSvg()) return;
+
+      this.httpClient.get(url, { responseType: "text" }).subscribe((svg) => {
+        const clean = DOMPurify.sanitize(svg, {
+          USE_PROFILES: { svg: true, svgFilters: true }
+        });
+        this.svgContent.set(
+          this.sanitizer.bypassSecurityTrustHtml(clean) as any
+        );
+      });
+    });
+  }
 }
