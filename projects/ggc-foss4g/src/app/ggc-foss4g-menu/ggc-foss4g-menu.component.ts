da6f2151ff34135306bd204c4809eb5a9ba0ed91
Dennis <dennis.geerdink@kadaster.nl>
2026-06-18 09:25:22 +0200

    voorbeeld 2 toolbar boven kaart

--- projects/ggc-foss4g/src/app/ggc-foss4g-menu/ggc-foss4g-menu.component.ts
+++ projects/ggc-foss4g/src/app/ggc-foss4g-menu/ggc-foss4g-menu.component.ts
@@ -1,27 +1,17 @@
 import { Component, inject } from "@angular/core";
 import { Router } from "@angular/router";
-import {
-  GgcLayerBrtAchtergrondkaartComponent,
-  GgcMapComponent
-} from "@kadaster/ggc-map";
-import { GgcSearchLocationComponent } from "@kadaster/ggc-search-location";
-
-export type ExampleLinks = "opdracht 1" | "opdracht 2" | "opdracht 3";
 
 @Component({
   selector: "app-ggc-foss4g-menu",
-  imports: [
-    GgcLayerBrtAchtergrondkaartComponent,
-    GgcMapComponent,
-    GgcSearchLocationComponent
-  ],
+  imports: [],
   templateUrl: "./ggc-foss4g-menu.component.html",
   styleUrl: "./ggc-foss4g-menu.component.scss"
 })
 export class GgcFoss4gMenuComponent {
-  private router: Router = inject(Router);
+  private readonly router: Router = inject(Router);
 
-  protected navagateTo(route: ExampleLinks) {
+  protected navigateTo(route: string) {
+    console.log(route);
     switch (route) {
       case "opdracht 1":
         this.router.navigate(["/opdracht1"]);
