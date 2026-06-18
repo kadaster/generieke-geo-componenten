69dd0ab8a8040f57a23a1d9d360ae0c16900feae
GroenendijkMax <max.groenendijk@kadaster.nl>
2026-06-18 12:38:25 +0200

    TMS-10217

--- projects/ggc-home/src/app/examples/example-search-location/example-search-location-only-location/example-search-location-only-location.component.ts
+++ projects/ggc-home/src/app/examples/example-search-location/example-search-location-only-location/example-search-location-only-location.component.ts
@@ -38,7 +38,7 @@ export class ExampleSearchLocationOnlyLocationComponent extends ExampleFormatCom
       "code/examples/example-search-location/example-search-location-only-location/example-search-location-only-location.png"
   } as ComponentInfo;
   urlComponentModule =
-    "example-search-location-only-location/example-search-location-only-location/example-search-location-only-location.component.ts";
+    "example-search-location/example-search-location-only-location/example-search-location-only-location.component.ts";
   tsDocsUrl = `${document.baseURI}tsdocs/classes/ggc-search-location_src_public-api.GgcSearchLocationComponent.html`;
   // DOCS-SKIP:END
   searchLocationOptions = {
