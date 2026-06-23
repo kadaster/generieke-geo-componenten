f4effadcba3bb1bb2d45ec14d26b2c08b952c801
Jordy Mackay <jordy.mackay@kadaster.nl>
2026-06-23 10:34:23 +0200

    alles in ggc-dataset-tree

--- projects/ggc-dataset-tree/src/elements/main.ts
+++ projects/ggc-dataset-tree/src/elements/main.ts
@@ -2,8 +2,10 @@ import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http"
 import { createCustomElement } from "@angular/elements";
 import { createApplication } from "@angular/platform-browser";
 import {GgcDatasetTreeComponent} from "../lib/dataset-tree/dataset-tree/ggc-dataset-tree.component";
+import {GgcMapComponent} from "@kadaster/ggc-map/src/lib/map/ggc-map.component";
 
 const GGC_DATASET_TREE_ELEMENT_TAG_NAME = "ggc-dataset-tree-element";
+const GGC_MAP_ELEMENT_TAG_NAME = "ggc-map-element";
 
 createApplication({
   providers: [provideHttpClient(withInterceptorsFromDi())]
@@ -13,6 +15,14 @@ createApplication({
       customElements.define(
         GGC_DATASET_TREE_ELEMENT_TAG_NAME,
         createCustomElement(GgcDatasetTreeComponent, {
+          injector: appRef.injector
+        })
+      );
+    }
+    if (!customElements.get(GGC_MAP_ELEMENT_TAG_NAME)) {
+      customElements.define(
+        GGC_MAP_ELEMENT_TAG_NAME,
+        createCustomElement(GgcMapComponent, {
           injector: appRef.injector
         })
       );
