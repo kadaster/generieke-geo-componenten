1b417508919b40562a059f280a4868b0337619b8
Jordy Mackay <jordy.mackay@kadaster.nl>
2026-06-30 09:40:31 +0200

    Revert "TMS-11248 cesium upgrade (#67)"
    This reverts commit 7534ef184393856f638baed62af42cc35002d142.
    
    # Conflicts:
    #	package-lock.json
    #	projects/ggc-map-3d/package.json

--- projects/ggc-map-3d/src/lib/service/core-selection.service.spec.ts
+++ projects/ggc-map-3d/src/lib/service/core-selection.service.spec.ts
@@ -181,9 +181,7 @@ describe("CoreSelectionService", () => {
     const cesiumMock = createCesiumMock() as Viewer;
 
     it("should call setHighlightOnFeature() and emit event when the type of feature is Cesium3DTileFeature", () => {
-      const pickedFeature = Object.create(
-        Cesium3DTileFeature.prototype
-      ) as Cesium3DTileFeature;
+      const pickedFeature = new Cesium3DTileFeature();
       service["viewer"] = cesiumMock;
       (cesiumMock.scene.pick as Mock).mockReturnValue(pickedFeature);
       const selection = createSelection(
@@ -398,7 +396,7 @@ describe("CoreSelectionService", () => {
       it("should add feature to selected when isSilhouetteActivated", () => {
         service["isSilhouetteActivated"] = true;
         const eventType = ScreenSpaceEventType.LEFT_CLICK;
-        const feature = {} as unknown as Cesium3DTileFeature;
+        const feature = new Cesium3DTileFeature();
         service["addToHighlightMap"](eventType);
         const silhouette = service["highlightMap"].get(eventType);
         service["setHighlightOnFeature"](feature, eventType);
@@ -426,7 +424,7 @@ describe("CoreSelectionService", () => {
     it("should remove feature from selected when isSilhouetteActivated", () => {
       service["isSilhouetteActivated"] = true;
       const eventType = ScreenSpaceEventType.LEFT_CLICK;
-      const feature = {} as unknown as Cesium3DTileFeature;
+      const feature = new Cesium3DTileFeature();
       service["addToHighlightMap"](eventType);
       const silhouette = service["highlightMap"].get(eventType);
       (silhouette as PostProcessStage).selected = [feature];
@@ -480,9 +478,7 @@ describe("CoreSelectionService", () => {
     it("should return a feature if ScreenSpaceEventType is MOUSE_MOVE", () => {
       const cesiumMock = createCesiumMock() as Viewer;
       service["viewer"] = cesiumMock;
-      const pickedFeature = Object.create(
-        Cesium3DTileFeature.prototype
-      ) as Cesium3DTileFeature;
+      const pickedFeature = new Cesium3DTileFeature();
       (cesiumMock.scene.pick as Mock).mockReturnValue(pickedFeature);
       const endPosition = new Cartesian2();
 
@@ -498,9 +494,7 @@ describe("CoreSelectionService", () => {
     it("should return a feature if ScreenSpaceEventType is LEFT_CLICK", () => {
       const cesiumMock = createCesiumMock() as Viewer;
       service["viewer"] = cesiumMock;
-      const pickedFeature = Object.create(
-        Cesium3DTileFeature.prototype
-      ) as Cesium3DTileFeature;
+      const pickedFeature = new Cesium3DTileFeature();
       (cesiumMock.scene.pick as Mock).mockReturnValue(pickedFeature);
       const position = new Cartesian2();
 
