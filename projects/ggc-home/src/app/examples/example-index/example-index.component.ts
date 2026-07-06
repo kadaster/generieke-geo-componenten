c2c76955c2e2bc26ac0b00cfa36700a82b1ef86d
Jordy Mackay <JordyMackay@users.noreply.github.com>
2026-07-06 14:29:24 +0200

    TMS-11044 quickstart (#71)
    * TMS-11044 Eerste opgezet quick-start
    
    * TMS-11044 vervolg quick start
    
    * TMS-11044 quickstart
    
    * URL navigatie voor quickstart, banner weggehaald
    
    * Inleiding toegevoegd, nieuwe quickstart afbeelding
    
    * heading inleiding
    
    * linting
    
    * TMS-11044 Feedback Eric (m.u.v. #3 en #5) verwerkt
    
    * Laatste feedbackpuntjes
    
    * feedback inleiding
    
    * feedback verwerkt, sommige voorbeelden simpeler gemaakt
    
    * linting
    
    * Grootste deel feedback verwerkt, plaatje bijgewerkt met nummers
    
    * juiste nummering
    
    * afsluitende pagina toegevoegd
    
    * nummers op consistente plek
    
    * feedback PR
    
    * TMS-11044 Feedback Ruben verwerkt
    
    dataset-tree en dataset-switcher naar de juiste component (i.p.v. thema)
    
    ---------
    
    Co-authored-by: Jordy Mackay <jordy.mackay@kadaster.nl>

--- projects/ggc-home/src/app/examples/example-index/example-index.component.ts
+++ projects/ggc-home/src/app/examples/example-index/example-index.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from "@angular/core";
+import { Component, inject, ViewEncapsulation } from "@angular/core";
 import { ExampleSearchLocationComponent } from "../example-search-location/example-search-location/example-search-location.component";
 import { ExampleSnappingBasicComponent } from "../example-snapping/example-snapping-basic/example-snapping-basic.component";
 import { ComponentInfo } from "../component-info.model";
@@ -53,6 +53,7 @@ import { Example3dSearchComponent } from "../example-3d/example-3d-search/exampl
 import { Example3dLayer3dTilesComponent } from "../example-3d/example-3d-layer-3d-tiles/example3d-layer-3d-tiles.component";
 import { Example3dLayerWmtsComponent } from "../example-3d/example-3d-layer-wmts/example3d-layer-wmts.component";
 import { Example3dLayerGeojsonComponent } from "../example-3d/example-3d-layer-geojson/example3d-layer-geojson.component";
+import { SessionStorageService } from "../../service/session-storage.service";
 
 interface GroupedCards {
   theme: string;
@@ -132,31 +133,26 @@ export class ExampleIndexComponent {
     new Example3dLayerWmtsComponent().componentInfo,
     new Example3dLayerGeojsonComponent().componentInfo
   ];
-
-  private readonly selectedComponentsKey = "selectedComponents";
-  private readonly selectedTagsKey = "selectedTags";
-  private readonly selectedThemesKey = "selectedThemes";
-  private readonly searchTermKey = "searchTerm";
+  private readonly sessionStorageService = inject(SessionStorageService);
 
   constructor() {
-    const storedSelectedThemes = sessionStorage.getItem(this.selectedThemesKey);
+    const storedSelectedThemes = this.sessionStorageService.getSelectedThemes();
     if (storedSelectedThemes) {
       this.selectedThemes = new Set(JSON.parse(storedSelectedThemes));
     }
 
-    const storedSelectedComponents = sessionStorage.getItem(
-      this.selectedComponentsKey
-    );
+    const storedSelectedComponents =
+      this.sessionStorageService.getSelectedComponents();
     if (storedSelectedComponents) {
       this.selectedComponents = new Set(JSON.parse(storedSelectedComponents));
     }
 
-    const storedSelectedTags = sessionStorage.getItem(this.selectedTagsKey);
+    const storedSelectedTags = this.sessionStorageService.getSelectedTags();
     if (storedSelectedTags) {
       this.selectedTags = new Set(JSON.parse(storedSelectedTags));
     }
 
-    const storedSearchTerm = sessionStorage.getItem(this.searchTermKey);
+    const storedSearchTerm = this.sessionStorageService.getSearchTerm();
     if (storedSearchTerm) {
       this.searchTerm = storedSearchTerm;
     }
@@ -215,7 +211,7 @@ export class ExampleIndexComponent {
 
   protected storeSearchTerm(value: string) {
     this.searchTerm = value;
-    sessionStorage.setItem("searchTerm", value);
+    this.sessionStorageService.setSearchTerm(value);
   }
 
   protected toggleTheme(theme: string): void {
@@ -225,9 +221,8 @@ export class ExampleIndexComponent {
       this.selectedThemes.add(theme);
     }
     this.selectedThemes = new Set(this.selectedThemes);
-    sessionStorage.setItem(
-      this.selectedThemesKey,
-      JSON.stringify(Array.from(this.selectedThemes))
+    this.sessionStorageService.setSelectedThemes(
+      Array.from(this.selectedThemes)
     );
   }
 
@@ -238,9 +233,8 @@ export class ExampleIndexComponent {
       this.selectedComponents.add(component);
     }
     this.selectedComponents = new Set(this.selectedComponents);
-    sessionStorage.setItem(
-      this.selectedComponentsKey,
-      JSON.stringify(Array.from(this.selectedComponents))
+    this.sessionStorageService.setSelectedComponents(
+      Array.from(this.selectedComponents)
     );
   }
 
@@ -251,10 +245,7 @@ export class ExampleIndexComponent {
       this.selectedTags.add(tag);
     }
     this.selectedTags = new Set(this.selectedTags);
-    sessionStorage.setItem(
-      this.selectedTagsKey,
-      JSON.stringify(Array.from(this.selectedTags))
-    );
+    this.sessionStorageService.setSelectedTags(Array.from(this.selectedTags));
   }
 
   protected resetFilters(): void {
@@ -262,22 +253,22 @@ export class ExampleIndexComponent {
     this.clearThemeFilter();
     this.clearTagFilter();
     this.searchTerm = "";
-    sessionStorage.removeItem(this.searchTermKey);
+    this.sessionStorageService.removeSearchTerm();
   }
 
   protected clearThemeFilter(): void {
     this.selectedThemes = new Set<string>();
-    sessionStorage.removeItem(this.selectedThemesKey);
+    this.sessionStorageService.removeSelectedThemes();
   }
 
   protected clearComponentFilter(): void {
     this.selectedComponents = new Set<string>();
-    sessionStorage.removeItem(this.selectedComponentsKey);
+    this.sessionStorageService.removeSelectedComponents();
   }
 
   protected clearTagFilter(): void {
     this.selectedTags = new Set<string>();
-    sessionStorage.removeItem(this.selectedTagsKey);
+    this.sessionStorageService.removeSelectedTags();
   }
 
   protected filteredCards(exclude?: string): ComponentInfo[] {
---
+++ projects/ggc-home/src/app/service/session-storage.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root"
+})
+export class SessionStorageService {
+  private readonly selectedComponentsKey = "selectedComponents";
+  private readonly selectedTagsKey = "selectedTags";
+  private readonly selectedThemesKey = "selectedThemes";
+  private readonly searchTermKey = "searchTerm";
+
+  getSelectedComponents() {
+    return sessionStorage.getItem(this.selectedComponentsKey);
+  }
+
+  getSelectedTags() {
+    return sessionStorage.getItem(this.selectedTagsKey);
+  }
+
+  getSelectedThemes() {
+    return sessionStorage.getItem(this.selectedThemesKey);
+  }
+
+  getSearchTerm() {
+    return sessionStorage.getItem(this.searchTermKey);
+  }
+
+  setSelectedComponents(selectedComponents: string[]) {
+    sessionStorage.setItem(
+      this.selectedComponentsKey,
+      JSON.stringify(selectedComponents)
+    );
+  }
+
+  setSelectedTags(selectedTags: string[]) {
+    sessionStorage.setItem(this.selectedTagsKey, JSON.stringify(selectedTags));
+  }
+
+  setSelectedThemes(selectedThemes: string[]) {
+    sessionStorage.setItem(
+      this.selectedThemesKey,
+      JSON.stringify(selectedThemes)
+    );
+  }
+
+  setSearchTerm(searchTerm: string) {
+    sessionStorage.setItem(this.searchTermKey, searchTerm);
+  }
+
+  removeSelectedComponents() {
+    sessionStorage.removeItem(this.selectedComponentsKey);
+  }
+
+  removeSelectedTags() {
+    sessionStorage.removeItem(this.selectedTagsKey);
+  }
+
+  removeSelectedThemes() {
+    sessionStorage.removeItem(this.selectedThemesKey);
+  }
+
+  removeSearchTerm() {
+    sessionStorage.removeItem(this.searchTermKey);
+  }
+
+  removeSessionStorage() {
+    this.removeSelectedComponents();
+    this.removeSelectedTags();
+    this.removeSelectedThemes();
+    this.removeSearchTerm();
+  }
+}
