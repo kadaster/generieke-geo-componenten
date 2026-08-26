365d3eaeb307656a1db525c27bc474717196ca5e
RubenKad <ruben.holubek@kadaster.nl>
2026-08-26 09:14:47 +0200

    chore: align imported changes with Angular 21

--- projects/ggc-map-3d/src/lib/service/ggc-selection.service.ts
+++ projects/ggc-map-3d/src/lib/service/ggc-selection.service.ts
@@ -1,8 +1,16 @@
 import { inject, Injectable } from "@angular/core";
-import { Color, ScreenSpaceEventType } from "@cesium/engine";
+import {
+  Cesium3DTileFeature,
+  Color,
+  Entity,
+  ScreenSpaceEventType
+} from "@cesium/engine";
 import { Observable } from "rxjs";
 import { CoreSelectionService } from "./core-selection.service";
-import { MapComponentEvent } from "@kadaster/ggc-models";
+import {
+  DEFAULT_CESIUM_MAPINDEX,
+  MapComponentEvent
+} from "@kadaster/ggc-models";
 import { SelectionConfig, SelectionEvent } from "../model/interfaces";
 
 /**
@@ -61,7 +69,22 @@ export class GgcSelectionService {
   }
 
   /**
-   * Verwijdert (destroy) een selectie voor een specifiek event type.
+   * Overschrijft de bestaande selectie met de meegegeven features.
+   *
+   * @param feature Het nieuwe feature dat geselecteerd wordt.
+   * @param selectIndex Optionele selectIndex (default: DEFAULT_CESIUM_MAPINDEX) waarop
+   * de selectie wordt overschreven.
+   */
+  setSelection(
+    feature: Cesium3DTileFeature | Entity,
+    selectIndex: string = DEFAULT_CESIUM_MAPINDEX
+  ) {
+    this.coreSelectionService.setSelection(feature, selectIndex);
+  }
+
+  /**
+   * Verwijdert (destroy) een selectie voor een specifiek event type. Er wordt niet meer geluisterd
+   * naar het `ScreenSpaceEventType`.
    *
    * @param eventType Het {@link ScreenSpaceEventType} waarvoor de selectie verwijderd moet worden
    */
